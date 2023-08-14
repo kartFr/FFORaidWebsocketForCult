0b906d8b4631272415ab883a38009eaa3bbd7cec
kartFr <94320656+kartFr@users.noreply.github.com>
2023-08-14 16:34:59 -0600

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -3,6 +3,7 @@ local TeleportService = game:GetService("TeleportService")
 local localPlayer = game.Players.LocalPlayer
 local socket
 local serverHopping = false
+local pong = false
 
 local success = pcall(function()
     socket = WebSocket.connect("ws://rapid-occipital-xenon.glitch.me/?" .. localPlayer.Name)
@@ -13,6 +14,11 @@ if not success then
 end
 
 local function hopServers(message)
+    if message == "pong" then
+        pong = true
+        return
+    end
+
     serverHopping = true
     localPlayer:Kick("hoppign to alek server :3")
     while task.wait(1) do
@@ -20,15 +26,16 @@ local function hopServers(message)
     end
 end
 
-socket.OnMessage:Connect(hopServers)
-
-socket.OnClose:connect(function()
+local function socketClose()
     if not serverHopping then
         socket = WebSocket.connect("ws://rapid-occipital-xenon.glitch.me/?" .. localPlayer.Name)
 
         socket.OnMessage:Connect(hopServers)
     end
-end)
+end
+
+socket.OnMessage:Connect(hopServers)
+socket.OnClose:Connect(socketClose)
 
 local playing
 
@@ -46,3 +53,18 @@ workspace.LiveChests.ChildAdded:connect(function(child)
         fireclickdetector(child:WaitForChild("ClickPart").ClickDetector)
     end
 end)
+
+while true do
+    pong = false
+    socket:Send("ping")
+
+    task.wait(5)
+
+    if not pong then
+        socket:Close()
+        socket = WebSocket.connect("ws://rapid-occipital-xenon.glitch.me/?" .. localPlayer.Name)
+
+        socket.OnMessage:Connect(hopServers)
+        socket.OnClose:Connect(socketClose)
+    end
+end
